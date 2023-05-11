521488152cac6d70f785c2b94f01e6c169a999ee
aabda2000 <aabda2000@gmail.com>
2023-05-11 18:54:52 +0200

    trigger

---
+++ TP/trigger.sql
@@ -0,0 +1,25 @@
+DROP table if exists users_audit;
+CREATE TABLE users_audit (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  changedat DATETIME DEFAULT NULL,
+  action VARCHAR(50) DEFAULT NULL
+);
+#CREATE TRIGGER before_users_update BEFORE
+UPDATE
+  ON users FOR EACH ROW
+INSERT INTO
+  users_audit
+SET
+  action = 'update',
+  username = OLD.username,
+  changedat = NOW();
+#Tester le trigger
+update
+  users
+set
+  username = "toto";
+select
+  *
+from
+  users_audit;
